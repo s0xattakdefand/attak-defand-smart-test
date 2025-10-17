dd97ab0f73adae47da84296da75bb4537bf8a9d2
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2025-10-17 11:30:42 +0700

    Add files via upload

---
+++ D/DVD-RW/DVD-RW.sol
@@ -0,0 +1,100 @@
+pragma solidity ^0.8.0;
+
+contract DVDRWAction {
+    address public owner;
+    mapping(string => uint256) public attackCounts;
+    mapping(string => uint256) public defendCounts;
+    mapping(string => bool) public wordExists;
+    string[] public words;
+    mapping(address => bool) public authorizedRewriters;
+
+    event WordSubmitted(address indexed user, string word);
+    event AttackPerformed(address indexed user, string word, uint256 count);
+    event DefendPerformed(address indexed user, string word, uint256 count);
+    event WordRewritten(address indexed user, string word, uint256 newAttackCount, uint256 newDefendCount);
+    event RewriterAuthorized(address indexed user);
+    event RewriterRevoked(address indexed user);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    modifier onlyAuthorized() {
+        require(msg.sender == owner || authorizedRewriters[msg.sender], "Not authorized to rewrite");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        authorizedRewriters[msg.sender] = true;
+    }
+
+    function submitWord(string memory _word) public {
+        require(bytes(_word).length > 0, "Word cannot be empty");
+        if (!wordExists[_word]) {
+            words.push(_word);
+            wordExists[_word] = true;
+        }
+        emit WordSubmitted(msg.sender, _word);
+    }
+
+    function attack(string memory _word) public {
+        require(wordExists[_word], "Word does not exist");
+        attackCounts[_word]++;
+        emit AttackPerformed(msg.sender, _word, attackCounts[_word]);
+    }
+
+    function defend(string memory _word) public {
+        require(wordExists[_word], "Word does not exist");
+        defendCounts[_word]++;
+        emit DefendPerformed(msg.sender, _word, defendCounts[_word]);
+    }
+
+    function rewriteWord(string memory _word, uint256 _newAttackCount, uint256 _newDefendCount) public onlyAuthorized {
+        require(wordExists[_word], "Word does not exist");
+        attackCounts[_word] = _newAttackCount;
+        defendCounts[_word] = _newDefendCount;
+        emit WordRewritten(msg.sender, _word, _newAttackCount, _newDefendCount);
+    }
+
+    function authorizeRewriter(address _user) public onlyOwner {
+        require(_user != address(0), "Invalid address");
+        authorizedRewriters[_user] = true;
+        emit RewriterAuthorized(_user);
+    }
+
+    function revokeRewriter(address _user) public onlyOwner {
+        require(_user != address(0), "Invalid address");
+        authorizedRewriters[_user] = false;
+        emit RewriterRevoked(_user);
+    }
+
+    function getWordCount() public view returns (uint256) {
+        return words.length;
+    }
+
+    function getAttackCount(string memory _word) public view returns (uint256) {
+        require(wordExists[_word], "Word does not exist");
+        return attackCounts[_word];
+    }
+
+    function getDefendCount(string memory _word) public view returns (uint256) {
+        require(wordExists[_word], "Word does not exist");
+        return defendCounts[_word];
+    }
+
+    function getAllWords() public view returns (string[] memory) {
+        return words;
+    }
+
+    function isAuthorizedRewriter(address _user) public view returns (bool) {
+        return authorizedRewriters[_user];
+    }
+
+    function resetWord(string memory _word) public onlyOwner {
+        require(wordExists[_word], "Word does not exist");
+        attackCounts[_word] = 0;
+        defendCounts[_word] = 0;
+    }
+}
